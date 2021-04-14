a3a6fdf913691216f67d17156f3670f7c0f9a6d7
Motaz <motaz@code.sd>
2021-04-14 23:35:28 +0200

    Add test file

---
+++ codeutils_test.go
@@ -0,0 +1,15 @@
+package codeutils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTitle(t *testing.T) {
+	aname, err := getConfigValue("test.ini", "name")
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+	} else {
+		fmt.Println("aname: ", aname)
+	}
+}
